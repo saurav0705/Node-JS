a3b5f72b29512b09fc17d4eaa87ca17616860674
saurav0705 <sauravaggarwal98@gmail.com>
2019-12-12 16:11:16 +0530

    static file through express

---
+++ Node-express/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+
+const host = 'localhost';
+const port = 8000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+"/public"));
+
+app.use(
+(req,res,next) => {
+    res.statusCode =200;
+    res.setHeader('Content-type','text/html');
+    res.end("<h1>this is an express server.</h1>");
+}
+);
+
+const server = http.createServer(app);
+server.listen(port,host,() => {
+    console.log(`server is up and running on http://${host}:${port}`);
+});
