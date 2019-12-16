7dbc2717388c0d7441e87be46e3334791fe6eee9
saurav0705 <sauravaggarwal98@gmail.com>
2019-12-16 17:36:29 +0530

    hell and promise application

--- Node-mongo/index.js
+++ Node-mongo/index.js
@@ -1,30 +1,41 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operation');
 
 
 const url = "mongodb+srv://root:root@cluster0-6jtqj.mongodb.net/test?retryWrites=true&w=majority";
-const dbName = "NodeJS_testDB";
-const client = new MongoClient(url, { useNewUrlParser: true });
-client.connect((err,client) => {
-        assert.equal(err,null);
-        console.log("Connected>>>>>>>>>>>>>");
+const dbName = "nodeJS_DB";
+const collection = "nodeJS_collection";
+//const client = new MongoClient(url, { useNewUrlParser: true });
+MongoClient.connect(url).then((client) => {
+        
+        console.log("Connecting to the server >>>>>>>>>>>>>");
         const db = client.db(dbName);
-        const collection = db.collection("Nodejs_testCollection");
-        collection.insert({"name":"test1","password":"test1_password"},(err,result) =>
-        {
-            assert.equal(err,null);
-            console.log("successfully inserted data.");
 
-            collection.find({}).toArray((err,allData) =>
-            {
-                assert.equal(err,null);
-                console.log("data of the collection is:: \n",allData);
-                db.dropCollection("Nodejs_testCollection", (err, result) => {
-                    assert.equal(err,null);
-                    console.log("deleted all data");
-                    client.close();
+        
+
+        dboper.insertDocument(db,{"name":"test1","description":"test1 description...."},collection)
+        .then((result) =>{
+            console.log("",result.ops);
+        
+
+            return dboper.updateDocument(db,{"name":"test1"},{"description":"updated......."},collection)
+                .then((result) =>{
+                    console.log("Updating documents ::::::: ",result.result);
+
+                    return  dboper.findDocuments(db,collection)
+                    .then((result) => {
+                        console.log("Finding docs :::::::::: ",result);
+
+                        return  db.dropCollection(collection)
+                        .then((result) => {
+                            console.log("dropped the whole collection ",result);
+
+                            return  client.close();
+                        });                        
+                     });                    
                 });
-            });
-        });
 
-});
+            }).catch((err) =>{console.log(err);});
+             
+}).catch((err) => { console.log("error connecting to the client ::: "+err)});
