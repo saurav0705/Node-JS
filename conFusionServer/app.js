1f11ded97be3e7b38a3667816dc457967bf38436
saurav0705 <sauravaggarwal98@gmail.com>
2019-12-19 17:21:32 +0530

    session in nodejs

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 const mongoose = require('mongoose');
 
 
@@ -30,10 +32,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('1234567890'));
+//app.use(cookieParser('1234567890'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
 var auth = function(req,res,next){
   var authHeader = req.headers.authorization;
-  if(!req.signedCookies.user)
+  if(!req.session.user)
   {
     if(!authHeader){
       res.statusCode = 401;
@@ -47,7 +58,7 @@ var auth = function(req,res,next){
     var user = auth[0];
     var pass = auth[1];
       if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed:true});
+        req.session.user = 'admin';
           next(); // authorized
       } else {
           var err = new Error('You are not authenticated!');
@@ -57,7 +68,7 @@ var auth = function(req,res,next){
     }
   }
   else{
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       next();
   }
   else {
